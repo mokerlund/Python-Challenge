f633703704e9a098f04ff336ef4e1a3a35fb0599
Megan <okerlund.megan@gmail.com>
2019-08-27 09:03:13 -0700

    Dictionary functionality is working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,7 +19,29 @@ election_csv_path = "PyPoll/election_data.csv"
 with open(election_csv_path, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
+    # Starting votes = 0
+    votes_cast = 0
+
     # Skip past header
     next(csv_reader)
 
+    candidates = {}
+
     for row in csv_reader:
+        
+        # Increment for each vote cast
+        votes_cast += 1
+
+        # Need to create a dictionary to add new name each time one comes up
+        # And add the value +1 per vote
+        if row[2] in candidates:
+            candidates[row[2]] += 1
+        else:
+            candidates.update({row[2]: 1})
+
+        # candidates = {candidate1:vote, candidate2:vote}
+        # candidates[candidate1] == vote
+    print(candidates)
+    print(votes_cast)
+
+    # winner = max(candidates[candidate1], candidates[candidate2], candidates[candidate3])
