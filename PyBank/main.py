5a4913261b6e822a772781198190b4f6f8247650
Megan <okerlund.megan@gmail.com>
2019-08-26 21:03:00 -0700

    Cleaned up code and corrected answers

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,9 @@
 #  The goal is to analyze and find:
-
 #   * The total number of months included in the dataset
-
 #   * The net total amount of "Profit/Losses" over the entire period
-
 #   * The average of the changes in "Profit/Losses" over the entire period
-
 #   * The greatest increase in profits (date and amount) over the entire period (Assuming Between Months)
-
 #   * The greatest decrease in losses (date and amount) over the entire period (Assuming Between Months)
-
 
 import csv
 
@@ -19,17 +13,20 @@ budget_csv_path = "budget_data.csv"
 with open(budget_csv_path, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     
+    # Skip past header
     next(csv_reader)
 
     # counter for months:
     months = 0
-
     # Running total of profit/loss
     total = 0
-
     # Assign highest and lowest beginning value
     high_increase = 0
     low_decrease = 0
+    # Assign a variable to store last month's profit/loss
+    prev_row_value = 0
+    # Assign Variable for average change
+    avg_total = 0
 
     # For each row, separate the first column into two
     for row in csv_reader:
@@ -37,25 +34,26 @@ with open(budget_csv_path, newline="") as csvfile:
         # Increment months and add totals 
         months += 1
         total += int(row[1])
-    
+
+        difference_value = int(row[1]) - prev_row_value
+
         # Find highest values
-        if (int(row[1]) > high_increase):
-             high_increase = int(row[1])
+        if (difference_value > high_increase):
+             high_increase = difference_value
              high_mon = row[0]
        
        # Find lowest values
-        if (int(row[1]) < low_decrease):
-            low_decrease = int(row[1])
+        if (difference_value < low_decrease):
+            low_decrease = difference_value
             low_mon = row[0]
-            
 
-    average = total / months
+
+        avg_total += difference_value
+
+        prev_row_value = int(row[1])
+        
+    average = round(avg_total / months, 2)
     
-    # print(average)
-    # print(low_decrease)
-    # print(months)
-    # print(total)
-    # print(row[1])
 
     ### Print Financial Analysis ###
     print("Financial Analysis")
